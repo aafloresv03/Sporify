ba20f7228cb6937c5b2fb1283469237868d4c7c8
Andrés Flores Velásquez <114762230+aafloresv03@users.noreply.github.com>
2025-11-06 21:06:15 +0100

    Integration fraagments

--- app/src/main/java/com/example/sporify/Home.java
+++ app/src/main/java/com/example/sporify/Home.java
@@ -10,31 +10,45 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.sporify.databinding.ActivityHomeBinding;
 
 public class Home extends AppCompatActivity {
 
     private ActivityHomeBinding binding;
+    private final Fragment homeFragment = new HomeFragment();
+    private final Fragment profileFragment = new ProfileFragment();
+    private final Fragment playlistFragment = new PlaylistFragment();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         binding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
 
         setSupportActionBar(binding.topToolBar);
+
+        replaceFragment(homeFragment);
+
+        Fragment selectedFragmente = null;
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
-            if (id == R.id.home)   { /* acción */ return true; }
-            if (id == R.id.profile){ /* acción */ return true; }
-            if (id == R.id.chats)  { /* acción */ return true; }
-            return false;
+            if (id == R.id.home)   {
+                replaceFragment(homeFragment);
+                return true;
+            }else if (id == R.id.profile){
+                replaceFragment(profileFragment);
+                return true;
+            }else if (id == R.id.playlist)  {
+                replaceFragment(playlistFragment);
+                return true;
+            }else {
+                return false;
+            }
         });
     }
 
@@ -48,10 +62,15 @@ public class Home extends AppCompatActivity {
         int id = item.getItemId();
         if(id == R.id.profile){
             return true;
-        } else if (id == R.id.settings) {
-            return true;
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    public void replaceFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container, fragment, null)
+                .addToBackStack(null);
     }
 }
