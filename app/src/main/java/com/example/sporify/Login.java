d0a544082a856ae967e46b8c6b622af1f3c16d9e
Andrés Flores Velásquez <114762230+aafloresv03@users.noreply.github.com>
2025-11-13 21:02:08 +0100

    Login xmls complete

--- app/src/main/java/com/example/sporify/Login.java
+++ app/src/main/java/com/example/sporify/Login.java
@@ -7,14 +7,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.example.sporify.databinding.ActivityLoginBinding;
+
 
 public class Login extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private final Fragment loginFragment = new LoginFragment();
+    private final Fragment registerFragment = new RegisterFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
